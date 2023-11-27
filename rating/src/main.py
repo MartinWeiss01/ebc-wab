175bb8ae6e3656f76430a13bfe8de556144f9f01
Martin Weiss <xweiss3@mendelu.cz>
2023-11-27 10:16:50 +0100

    Add rating servicer to server

--- rating/src/main.py
+++ rating/src/main.py
@@ -1,8 +1,10 @@
 import grpc
 import concurrent import futures
+import .rating_pb2_grpc as rating_pb2_grpc
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    rating_pb2_grpc.add_RatingServicer_to_server(rating_pb2_grpc.RatingServicer(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
 
